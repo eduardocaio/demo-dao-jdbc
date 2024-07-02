e3fba3c35c3098b65b92682403ade957a11b7536
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-07-02 12:26:45 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,5 +41,12 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=====TESTE 5: seller update=====");
+
+        seller = sellerDao.findById(8);
+        seller.setBaseSalary(7500.0);
+        sellerDao.update(seller);
+        System.out.println("Updated!");
+
     }
 }
